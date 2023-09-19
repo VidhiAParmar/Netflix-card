62ccda75258b383dffecafc1c357022046edd690
VidhiAParmar <parmaravidhi@gmail.com>
2023-09-19 14:55:09 +0530

    It shows netflix series

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Card from './Cards';
+import SData from './SData';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = ()=>(
+  <>
+    <h1 className='heading_style'> The Netflix Series </h1>
+    {SData.map((val)=>{
+       return(
+        <Card
+        key={val.id}
+        imgsrs={val.imgsrs}
+        title={val.title}
+        sname={val.sname}
+        link={val.link}
+        />
+      );
+      })
+    }
+  </>
+);
 
 export default App;
---
+++ src/SData.jsx
@@ -0,0 +1,25 @@
+const SData = [
+    {
+        id:1,
+        imgsrs :'https://occ-0-4110-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABR-NiIiYmGpicmMLMEOomctuX-NDk9WDj2OZnxP2JRjuXnq47nC_hBo4-xgbjuQSX5hRgAvMEI5V83N2yiBKIBQnXpNMH_VW-0M.jpg?r=a75',
+        title: " Netflix Original Series ",
+        sname: " The Karate Kid ",
+        link: 'https://www.netflix.com/in/title/70125931'
+    },
+    {
+        id:2,
+        imgsrs: 'https://occ-0-4110-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABUuwa85zjDiGaUURrexIT0fgRozPOPQPJt2z4OtxHKJeaKSIBEngi4wDNklqQqm0x2oSDFgRaDg9fDeihLDMIl378es1pPtCI7g.jpg?r=860',
+        title: " Netflix Original Series ",
+        sname: " The Boss Baby ",
+        link: 'https://www.netflix.com/in/title/80146758'
+    },
+    {
+        id:3,
+        imgsrs: 'https://occ-0-4110-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABR-NiIiYmGpicmMLMEOomctuX-NDk9WDj2OZnxP2JRjuXnq47nC_hBo4-xgbjuQSX5hRgAvMEI5V83N2yiBKIBQnXpNMH_VW-0M.jpg?r=a75',
+        title: " Netflix Original Series ",
+        sname: " The Karate Kid ",
+        link: 'https://www.netflix.com/in/title/70125931'
+    }
+
+];
+export default SData;
